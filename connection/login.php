15ed73c4a9c566d73f46575a779b4a4da259cb71
Lioniok <lionioking1@gmail.com>
2018-05-07 11:50:56 +0600

    Login connection has been done

--- connection/login.php
+++ connection/login.php
@@ -11,12 +11,17 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+$email = $_POST["email"];
+$password = $_POST["password"];
+
 
 // prepare and bind
 $stmt = $conn->prepare("SELECT id from user where email=? AND password=?");
 $stmt->bind_param("ss",$email,$password);
 $stmt->execute();
-echo "New records created successfully";
+$stmt->bind_result($id);
+$stmt->fetch();
+echo "New records created successfully". $id;
 $stmt->close();
 $conn->close();
 ?>
