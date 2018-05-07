0dd65fa629ccbf6b33b2721b6d788783507ba9c0
Lioniok <lionioking1@gmail.com>
2018-05-07 11:34:41 +0600

    Update Query in Login

--- connection/login.php
+++ connection/login.php
@@ -13,17 +13,10 @@ if ($conn->connect_error) {
 }
 
 // prepare and bind
-$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
-$stmt->bind_param("sss", $firstname, $lastname, $email);
-
-// set parameters and execute
-$firstname = "John";
-$lastname = "Doe";
-$email = "john@example.com";
+$stmt = $conn->prepare("SELECT id from user where email=? AND password=?");
+$stmt->bind_param("ss",$email,$password);
 $stmt->execute();
-
 echo "New records created successfully";
-
 $stmt->close();
 $conn->close();
 ?>
