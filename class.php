abb155e220df425c12d5789d9a5cfa94151381de
sadik061 <sadik061@gmail.com>
2018-05-15 02:52:07 +0600

    update after a long time

--- class.php
+++ class.php
@@ -3,7 +3,7 @@
 <?php include 'templates/navbar.php';?>
 <?php include 'templates/sidebar.php';?>
 
-<div id="page-wrapper" style="padding-top: 2%;">
+<div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">Classes</h1>
@@ -15,52 +15,64 @@
         <div class="col-lg-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
+                    Ask a question
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <form action="../study/connection/createclass.php" method="post">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="topic" name="topic" type="text" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="date" name="date" type="text" autofocus>
+                                </div>
+
+                                <input type="Submit" class="btn btn-success btn-outline" placeholder="Create class">
+                            </fieldset>
+
+                    </form>
+
+                </div>
+                <!-- /.panel-body -->
+            </div>
+        </div>
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
                     DataTables Advanced Tables
                 </div>
                 <!-- /.panel-heading -->
                 <div class="panel-body">
-                    <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                    <table width="100%" class="table">
                         <thead>
                         <tr>
-                            <th>#</th>
                             <th>Topic</th>
                             <th>Date</th>
-                            <th>Questions</th>
-                            <th>Action</th>
+
                         </tr>
                         </thead>
                         <tbody>
-                        <tr class="odd gradeX">
-                            <td>1</td>
-                            <td>What is system design analysis and design</td>
-                            <td>03-03-2018</td>
-                            <td class="center">8</td>
-                            <td class="center"><button type="button" class="btn btn-outline btn-success">Success</button></td>
-                        </tr>
-                        <tr class="even gradeC">
-                            <td>2</td>
-                            <td>SDLC</td>
-                            <td>05-03-2018</td>
-                            <td class="center">10</td>
-                            <td class="center"><button type="button" class="btn btn-outline btn-success">Success</button></td>
-                        </tr>
-                        <tr class="odd gradeA">
-                            <td>3</td>
-                            <td>Feasibility test</td>
-                            <td>09-03-2018</td>
-                            <td class="center">18</td>
-                            <td class="center"><button type="button" class="btn btn-outline btn-success">Success</button></td>
-                        </tr>
-                        <tr class="even gradeA">
-                            <td>4</td>
-                            <td>Class diagram</td>
-                            <td>11-03-2018</td>
-                            <td class="center">10</td>
-                            <td class="center"><button type="button" class="btn btn-outline btn-success">Success</button></td>
-                        </tr>
+                        <?php
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $dbname = "study";
+                        error_reporting(1);
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
+                        }
+                        $sql = "SELECT * FROM class where classroomid=".$_SESSION["classroomid"];
+                        $result = mysqli_query($conn, $sql);
+                        if ($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                echo '<td class="odd gradeX">'.$row["topic"].'</td><td>'.$row["datee"].'</td><td class="center"><a href="viewclass.php" class="btn btn-outline btn-success" role="button" aria-pressed="true">View</a></td></tr>';
+                            }
+                        }
+                        $conn->close();
 
-
-
+                        ?>
                         </tbody>
                     </table>
                     <!-- /.table-responsive -->
@@ -74,4 +86,5 @@
     </div>
 </body>
 <?php include 'templates/footer.php';?>
+<?php include 'templates/classroomcreate.php';?>
 </html>
